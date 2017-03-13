cadec19f8f030e3d2e81a90772d29a84e4b84bba
Mitchell Blom <mitchellblom@gmail.com>
2017-03-13 14:01:18 -0500

    info writing well to dom but not in table format

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # NSS IFFE Bootstrap Exercise
 
 <!-- 1. HTML create form -->
-2. Random hardcode for styling
-3. Add basic Bootstrap styling
-4. Event listeners in DOM handler
-5. Log EL's
-6. Write IIFE to accept info
-7. Link input fields with IIFE
-8. Validate Amount $ input and that all fields are complete
-9. Cancel button functionality 
-10. Apply grid system per device
-11. Alternate striping for table entries
+<!-- 1. Random hardcode for styling -->
+<!-- 1. Add basic Bootstrap styling -->
+<!-- 1. Write IIFE to accept info -->
+<!-- 1. Alternate striping for table entries -->
+1. Event listeners in DOM handler
+1. Log EL's
+1. Link input fields with IIFE
+1. Validate Amount $ input and that all fields are complete
+1. Apply Cancel button functionality 
+1. Apply grid system per device
+1. Add Frequency/radio button functionality
+1. Clear input fields upon Enter or Donate button click
+1. Complete Bootstrap requirements
--- js/donorInfo.js
+++ js/donorInfo.js
@@ -10,13 +10,30 @@ var DonorInfo = (function() {
 				return donorArray;
 			},
 			printToTable: function(newDonor) {
-				var table = document.getElementById("table");
-				var tableString = `<th>${newDonor.name}</th>` 
-				tableString += `<tr>${newDonor.email}</tr>`;
-				tableString += `<tr>${newDonor.amount}</tr>`;
-				for (var i = 0; i < donorArray.length; i++){
-					table.innerHTML += donorArray[i].name;
+				var writeToTable = document.getElementById("donor-info-goes-here");
+				// var tableString = `<tr>` 
+				// tableString += `<td>${newDonor.name}</td>`;
+				// tableString += `<td>${newDonor.email}</td>`;
+				// tableString += `<td>${newDonor.amount}</td>`;
+				// tableString += `<td>${newDonor.amount}</td>`;
+				// tableString += `</tr>` 
+				for (var i = 0; i < 1; i++){
+					writeToTable.innerHTML += `<tr>`;
+					writeToTable.innerHTML += `<td>${donorArray[i].name}</td>`;
+					writeToTable.innerHTML += `<td>${donorArray[i].email}</td>`;
+					writeToTable.innerHTML += `<td>${donorArray[i].amount}</td>`;
+					writeToTable.innerHTML += `</tr>` ;
 				}
 			}
 		}
 })();
+
+
+
+
+// 						<tr>
+// 				   			<td>Jane Doe</td>
+// 				   			<td>jdoe2@deer.com</td>
+// 				   			<td>$88</td>
+// 				   			<td>Lump Sum</td>
+//  					</tr>
